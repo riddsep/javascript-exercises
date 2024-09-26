9f4dc3a39ba565fb24abaac4d8349773e34083b6
riddsep <ridoseptiawan00@gmail.com>
2024-09-26 16:23:44 +0700

    Solution 06 leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
+// Tahun kabisat adalah tahun yang dapat dibagi 4
+// tahun yang dapat dibagi 100 bukanlah tahun kabisat
+// kecuali jika tahun tersebut juga dapat dibagi 400
 
+const leapYears = function (year) {
+  const isYearDivisibleByFour = year % 4 === 0;
+  const isCentury = year % 100 === 0;
+  const isYearDivisibleByFourHundred = year % 400 === 0;
+
+  if (isYearDivisibleByFour && (!isCentury || isYearDivisibleByFourHundred)) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
